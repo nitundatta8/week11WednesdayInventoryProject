bef1bd44bbdc8b082aceaf1a5814de6b7aa4e75f
nitunwa <nitun.2@gmail.com>
2020-05-27 12:03:25 -0700

    add controllers both Home and Inventory and populated the code inside them too

---
+++ Inventory/Controllers/InventoryController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Inventory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventory.Controllers
+{
+  public class InventoryController : Controller
+  {
+    private readonly InventoryContext _db;
+
+    public ItemsController(InventoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Product> model = _db.Inventories.ToList();
+      return View(model);
+    }
+
+    public ActionResult Details(int id)
+    {
+      Product thisProduct = _db.Products.FirstOrDefault(products => products.InventoryId == id);
+      return View(thisProduct);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Product product)
+    {
+      _db.Inventories.Add(product);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+
+  }
+}
